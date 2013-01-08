fa298cf7a601ef0bc32f134f4efa124aac20f514
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2013-01-08 14:50:27 +0000

    fixed a slight comm.c bug with regen
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@169 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- act.wizard.c
+++ act.wizard.c
@@ -1074,8 +1074,9 @@ static void do_stat_character(struct char_data *ch, struct char_data *k)
                 compute_damage_reduction(k, -1),
                 compute_concealment(k));
 
-  send_to_char(ch, "\tCStoneskin: \tn%d\tC, Mirror Images: \tn%d\tC, Spell Resist: \tn%d\r\n", 
-	GET_STONESKIN(k), GET_IMAGES(k), GET_SPELL_RES(k));
+  send_to_char(ch, "\tCStoneskin: \tn%d\tC, Mirror Images: \tn%d\tC, Cloudkill:"
+                   " \tn%d\tC, Spell Resist: \tn%d\r\n", 
+	GET_STONESKIN(k), GET_IMAGES(k), CLOUDKILL(k), GET_SPELL_RES(k));
 
   send_to_char(ch, "\tCMemming? \tn%d\tC, Praying? \tn%d\tC, Communing? \tn%d\r\n", 
 	PRAYIN(k, 2), PRAYIN(k, 0), PRAYIN(k, 1));
