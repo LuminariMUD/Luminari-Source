d06b686ffebc84c86cab2fe0d5265f156cb718a0
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2013-01-13 05:56:31 +0000

    group fix
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@189 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- mud_event.c
+++ mud_event.c
@@ -185,6 +185,9 @@ void free_mud_event(struct mud_event_data *pMudEvent)
   struct descriptor_data * d;
   struct char_data * ch;
 
+  if (!pMudEvent)
+    return;
+
   switch (mud_event_index[pMudEvent->iId].iEvent_Type) {
     case EVENT_WORLD:
       remove_from_list(pMudEvent->pEvent, world_events);
