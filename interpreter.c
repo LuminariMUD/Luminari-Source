cac96368946df60b4e49680a87659f5f5de2b6c1
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2013-01-23 16:31:57 +0000

    fixed mag_summon messages
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@271 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- interpreter.c
+++ interpreter.c
@@ -147,8 +147,8 @@ cpp_extern const struct command_info cmd_info[] = {
   { "create"  , "create"     , POS_STANDING, do_not_here , 1, 0 },
   { "checkcraft"  , "checkcraft"     , POS_STANDING, do_not_here , 1, 0 },
   { "convert"  , "covert"     , POS_STANDING, do_not_here , 1, 0 },
-  { "crystalfist" , "crystalfist"    , POS_FIGHTING , do_crystalfist , 0, 0 },
-  { "crystalbody" , "crystalbody"    , POS_FIGHTING , do_crystalbody , 0, 0 },
+  { "crystalfist" , "crystalf"    , POS_FIGHTING , do_crystalfist , 0, 0 },
+  { "crystalbody" , "crystalb"    , POS_FIGHTING , do_crystalbody , 0, 0 },
   
   { "date"     , "da"      , POS_DEAD    , do_date     , 1, SCMD_DATE },
   { "dc"       , "dc"      , POS_DEAD    , do_dc       , LVL_GOD, 0 },
@@ -291,7 +291,7 @@ cpp_extern const struct command_info cmd_info[] = {
   { "purify"   , "purify"  , POS_FIGHTING, do_purify   , 1, 0 },  
   { "purge"    , "purge"   , POS_DEAD    , do_purge    , LVL_BUILDER, 0 },
   { "prayer"   , "prayer"  , POS_RESTING , do_gen_memorize , 0, SCMD_PRAY },
-  { "petition" , "petition", POS_RESTING , do_gen_memorize , 0, SCMD_PETITION },
+  { "petition" , "peti", POS_RESTING , do_gen_memorize , 0, SCMD_PETITION },
 
   { "qedit"    , "qedit"   , POS_DEAD    , do_oasis_qedit, LVL_BUILDER, 0 },
   { "qlist"    , "qlist"   , POS_DEAD    , do_oasis_list, LVL_BUILDER, SCMD_OASIS_QLIST },
