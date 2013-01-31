1dcab109e10bf2103586e3fa8e1f4628bdfd29f1
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2013-01-31 18:51:08 +0000

    added another parameter to save_char() for saving events
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@338 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- players.c
+++ players.c
@@ -870,7 +870,7 @@ void save_char(struct char_data * ch, int mode)
   /* save_char(x, 1) will skip this block (i.e. not saving events)
      this is necessary due to clearing events that occurs immediately
      before extract_char_final() in extract_char() */
-  if (!mode) {
+  if (mode != 1) {
     /* Save events */
     /* Not going to save every event */
     fprintf(fl, "Evnt:\n");
