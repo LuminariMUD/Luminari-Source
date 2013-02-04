558f6516ad3be94bde5ef665acff18fcc7f02502
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2013-02-04 18:57:59 +0000

    bug with 'beckon'
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@385 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- act.other.c
+++ act.other.c
@@ -121,7 +121,7 @@ ACMD(do_call)
         return;
       } else {
         char_from_room(k->follower);
-        char_to_room(k->follower, real_room(IN_ROOM(ch)));        
+        char_to_room(k->follower, IN_ROOM(ch));        
         act("$n calls $N!", FALSE, ch, 0, k->follower, TO_ROOM);
         act("You call forth $N!", FALSE, ch, 0, k->follower, TO_CHAR);
         return;
