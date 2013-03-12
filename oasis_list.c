8c802107a966d29700b219e23ff057f1fd3b2987
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2013-03-12 04:40:19 +0000

    debugging
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@713 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- oasis_list.c
+++ oasis_list.c
@@ -622,7 +622,7 @@ static void list_rooms(struct char_data *ch, zone_rnum rnum, room_vnum vmin, roo
       counter++;
 
         len += snprintf(buf + len, sizeof(buf) - len, "%4d)%s [%s%-5d%s] %s%-*s%s %s",
-                          counter, (has_zcmds != NULL ? (has_zcmds[i] == TRUE ? "Z" : " ") : ""), QGRN, world[i].number, QNRM,
+                          counter, (has_zcmds != NULL ? (has_zcmds[counter] == TRUE ? "Z" : " ") : ""), QGRN, world[i].number, QNRM,
                           QCYN, count_color_chars(world[i].name)+44, world[i].name, QNRM,
                           world[i].proto_script ? "[TRIG] " : ""
                           );
