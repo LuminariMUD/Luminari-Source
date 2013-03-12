e3075359c7a0a0abc1df322896470d0860f0b47d
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2013-03-12 03:50:10 +0000

    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@707 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- oasis_list.c
+++ oasis_list.c
@@ -594,7 +594,7 @@ static void list_rooms(struct char_data *ch, zone_rnum rnum, room_vnum vmin, roo
   if (!top_of_world)
     return;
 
-  CREATE(has_zcmds, bool, top - bottom);
+  /*CREATE(has_zcmds, bool, top - bottom);
   while (ZCMD(real_zone(bottom), zscmd).command != 'S') {
     switch (ZCMD(real_zone(bottom), zscmd).command)
     {
@@ -610,14 +610,14 @@ static void list_rooms(struct char_data *ch, zone_rnum rnum, room_vnum vmin, roo
     zscmd++;
     if (temp_num >= bottom && temp_num <= top)
       has_zcmds[temp_num - bottom] = TRUE;
-  }
+  } */
   for (i = 0; i <= top_of_world; i++) {
     /** Check to see if this room is one of the ones needed to be listed.    **/
     if ((world[i].number >= bottom) && (world[i].number <= top)) {
       counter++;
 
-        len += snprintf(buf + len, sizeof(buf) - len, "%4d)%1s [%s%-5d%s] %s%-*s%s %s",
-                          counter, (has_zcmds != NULL ? (has_zcmds[i] == TRUE ? "Z" : " ") : ""), QGRN, world[i].number, QNRM,
+        len += snprintf(buf + len, sizeof(buf) - len, "%4d) [%s%-5d%s] %s%-*s%s %s",
+                          counter, /*(has_zcmds != NULL ? (has_zcmds[i] == TRUE ? "Z" : " ") : ""),*/ QGRN, world[i].number, QNRM,
                           QCYN, count_color_chars(world[i].name)+44, world[i].name, QNRM,
                           world[i].proto_script ? "[TRIG] " : ""
                           );
