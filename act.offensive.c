bb1e7acf5f2239ec1fdf0203342b27e463673517
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2013-04-24 18:59:37 +0000

    commented out flee lag
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@1275 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- act.offensive.c
+++ act.offensive.c
@@ -1520,9 +1520,8 @@ ACMD(do_flee) {
       return;
     }
   }
-  // half a second for non newbies
-  if (GET_LEVEL(ch) <= LEVEL_NEWBIE)
-  SET_WAIT(ch, 50);
+  /* trying without delay */
+  //SET_WAIT(ch, 10);
 }
 
 
