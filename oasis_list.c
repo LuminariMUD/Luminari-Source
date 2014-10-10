5f76d18d777ada896479b0de8aa2638158e25f47
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2014-10-10 15:02:44 +0000

    fix bug in 'olist worn'
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@1540 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- oasis_list.c
+++ oasis_list.c
@@ -281,7 +281,7 @@ void perform_obj_type_list(struct char_data * ch, char *arg) {
 
 /* this function is ran for doing:  olist worn <slot> */
 void perform_obj_worn_list(struct char_data *ch, char *arg) {
-  int num, wearloc, found = 0, len = 0, tmp_len = 0, i = 0, count = 0;
+  int num, wearloc, found = 0, len = 0, tmp_len = 0, i = 0;
   obj_vnum ov;
   char buf[MAX_STRING_LENGTH], bitbuf[MEDIUM_STRING];
   struct obj_data *obj = NULL;
@@ -343,8 +343,7 @@ void perform_obj_worn_list(struct char_data *ch, char *arg) {
     }
     
     /* another dummy check */
-    count++;
-    if (count >= 700) {
+    if (found >= 700) {
       tmp_len = snprintf(buf + len, sizeof (buf) - len, "**OVERLOADED BUFF***\r\n");
       len += tmp_len;
       break;
