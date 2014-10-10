cf6718fbfaff45064865cad3855c4f22c929f751
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2014-10-10 08:14:36 +0000

    fix bug in 'olist worn'
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@1527 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- oasis_list.c
+++ oasis_list.c
@@ -286,7 +286,7 @@ void perform_obj_worn_list(struct char_data *ch, char *arg) {
 
   wearloc = atoi(arg);
   
-  if (wearloc > (NUM_ITEM_WEARS + 1)) {
+  if (wearloc > NUM_ITEM_WEARS || wearloc < 0) {
     send_to_char(ch, "Out of bounds\r\n");
     return;
   }
