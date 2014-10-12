9a981f2d0bd9c492970728f9a801b7c377eaa081
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2014-10-12 00:12:07 +0000

    start work on "eqrating" command
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@1556 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- act.wizard.c
+++ act.wizard.c
@@ -6161,10 +6161,11 @@ ACMD(do_eqrating) {
     tmp_len = snprintf(buf + len, sizeof (buf) - len, "\r\n");
     len += tmp_len;
     
-    if (j >= 700) {
+    /* yeah we have to cap things, wish i had a better solution at this stage!
+     -zusuk */
+    if (i >= 700) {
       tmp_len = snprintf(buf + len, sizeof (buf) - len, "**OVERLOADED BUFF***\r\n");
       len += tmp_len;
-      
       break;
     }
   }
