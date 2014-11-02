b670de38fec92c6013982a884c013a0a6e19062a
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2014-11-02 18:01:37 +0000

    start integrating the new trap system courtesy of hl
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@1714 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- oasis_list.c
+++ oasis_list.c
@@ -179,9 +179,12 @@ void perform_obj_type_list(struct char_data * ch, char *arg) {
             }
             switch (v1) {
               case TRAP_TYPE_ENTER_ROOM: /* display effect and difficulty */
-                if (v2 >= TRAP_EFFECT_FIRST_VALUE) { /* not a normal spell effect */
+                if (v3 >= TRAP_EFFECT_FIRST_VALUE) { /* not a normal spell effect */
                   tmp_len = snprintf(buf + len, sizeof (buf) - len, "%s%3d%s) %7d %s%s | Trap effect: %s | Trap difficulty: %d\r\n",
                       QGRN, ++found, QNRM, ov, obj_proto[r_num].short_description, QNRM, trap_effects[1000-v3], v4);
+                } else { /* spell effect */
+                  tmp_len = snprintf(buf + len, sizeof (buf) - len, "%s%3d%s) %7d %s%s | Trap spell: %s | Trap difficulty: %d\r\n",
+                      QGRN, ++found, QNRM, ov, obj_proto[r_num].short_description, QNRM, spell_info[v3].name, v4);                  
                 }
                 break;
               case TRAP_TYPE_OPEN_DOOR:
