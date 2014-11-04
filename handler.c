898a4a6fda5deedfd12f10a36cc3aa1dd3de5cd4
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2014-11-04 12:23:31 +0000

    Damage reduction changes
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@1749 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- handler.c
+++ handler.c
@@ -104,7 +104,7 @@ int isname(const char *str, const char *namelist) {
 }
 
 /* modify a character's given apply-type (loc) by value */
-void aff_apply_modify(struct char_data *ch, byte loc, sbyte mod, char *msg) {
+void aff_apply_modify(struct char_data *ch, byte loc, sbyte mod, char *msg) {  
   switch (loc) {
 
     case APPLY_STR:
