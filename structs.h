c220b440f0ea764ae981015e2ed23bbdca9e99ef
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2014-11-14 09:43:40 +0000

    Formatting errors in affects
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@1858 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- structs.h
+++ structs.h
@@ -2627,7 +2627,7 @@ struct affected_type {
   byte location; /**< Tells which ability to change(APPLY_XXX). */
   int bitvector[AF_ARRAY_MAX]; /**< Tells which bits to set (AFF_XXX). */
 
-  void *data; /**< Extra data for various locations (e.g. APPLY_DR). */
+  int bonus_type; /**< What type of bonus (if this is a bonus) is this. */
   
   struct affected_type *next; /**< The next affect in the list of affects. */
 };
