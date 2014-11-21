1357ebaf68fe2ac15e989b22c4b61f20ec44663a
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2014-11-21 09:04:48 +0000

    renamed project due to confusion issues on my local system -zusuk :P
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@1932 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- nbproject/Package-Default.bash
+++ nbproject/Package-Default.bash
@@ -14,7 +14,7 @@ NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
 OUTPUT_PATH=MissingOutputInProject
 OUTPUT_BASENAME=MissingOutputInProject
-PACKAGE_TOP_DIR=trunk/
+PACKAGE_TOP_DIR=LuminariMUDProject/
 
 # Functions
 function checkReturnCode
@@ -59,15 +59,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/trunk"
+makeDirectory "${NBTMPDIR}/LuminariMUDProject"
 copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/trunk.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/LuminariMUDProject.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/trunk.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/LuminariMUDProject.tar *
 checkReturnCode
 
 # Cleanup
