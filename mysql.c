1dfc856e8304438921b77b5bda668c7a3dc199e0
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2014-12-15 12:18:36 +0000

    Added PATHS!
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@2060 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- mysql.c
+++ mysql.c
@@ -340,9 +340,9 @@ void load_paths() {
     path_table[i].num_vertices = atoi(row[4]);
     path_table[i].path_props   = atoi(row[6]);
     
-    path_table[i].glyphs[GLYPH_TYPE_NS]  = strdup(row[7]);
-    path_table[i].glyphs[GLYPH_TYPE_EW]  = strdup(row[8]);
-    paht_table[i].glyphs[GLYPH_TYPE_INT] = strdup(row[9]);
+    path_table[i].glyphs[GLYPH_TYPE_PATH_NS]  = strdup(row[7]);
+    path_table[i].glyphs[GLYPH_TYPE_PATH_EW]  = strdup(row[8]);
+    paht_table[i].glyphs[GLYPH_TYPE_PATH_INT] = strdup(row[9]);
 
     /* Parse the polygon text data to get the vertices, etc.
        eg: LINESTRING(0 0,10 0,10 10,0 10,0 0) */
