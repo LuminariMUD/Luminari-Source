eecbd51c0b2df25fe8c47d853fe0609131ce8b0c
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2015-05-31 16:02:04 +0000

    adding some unimplemented feats, sorting of feats in the code, and some minor bug fixes
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@2674 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- magic.c
+++ magic.c
@@ -4283,6 +4283,7 @@ void mag_summons(int level, struct char_data *ch, struct obj_data *obj,
       GET_REAL_DEX(mob) += CLASS_LEVEL(ch, CLASS_WIZARD) / 6 + 1;
       GET_REAL_AC(mob) += (CLASS_LEVEL(ch, CLASS_WIZARD) / 6 + 1) * 10;
       GET_REAL_MAX_HIT(mob) += (CLASS_LEVEL(ch, CLASS_WIZARD) / 10 + 1) * GET_LEVEL(mob); /* con bonus */
+      GET_HIT(mob) = GET_REAL_MAX_HIT(mob);
     } else if (GET_SPECIALTY_SCHOOL(ch) == NECROMANCY &&
         ( spellnum == SPELL_ANIMATE_DEAD ||
           spellnum == SPELL_GREATER_ANIMATION ||
@@ -4293,7 +4294,7 @@ void mag_summons(int level, struct char_data *ch, struct obj_data *obj,
       GET_REAL_CON(mob) += CLASS_LEVEL(ch, CLASS_WIZARD) / 6 + 1;
       GET_REAL_DEX(mob) += CLASS_LEVEL(ch, CLASS_WIZARD) / 6 + 1;
       GET_REAL_AC(mob) += (CLASS_LEVEL(ch, CLASS_WIZARD) / 6 + 1) * 10;
-      GET_REAL_MAX_HIT(mob) += (CLASS_LEVEL(ch, CLASS_WIZARD) / 10 + 1) * GET_LEVEL(mob); /* con bonus */
+      GET_HIT(mob) = GET_REAL_MAX_HIT(mob);
     }
 
     act(mag_summon_msgs[msg], FALSE, ch, 0, mob, TO_ROOM);
