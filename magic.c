6ecbe9290bbd9514c486ff21e2d8f91b9640e363
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2015-05-31 15:31:39 +0000

    adding some unimplemented feats, sorting of feats in the code, and some minor bug fixes
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@2673 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- magic.c
+++ magic.c
@@ -4283,7 +4283,7 @@ void mag_summons(int level, struct char_data *ch, struct obj_data *obj,
       GET_REAL_DEX(mob) += CLASS_LEVEL(ch, CLASS_WIZARD) / 6 + 1;
       GET_REAL_AC(mob) += (CLASS_LEVEL(ch, CLASS_WIZARD) / 6 + 1) * 10;
       GET_REAL_MAX_HIT(mob) += (CLASS_LEVEL(ch, CLASS_WIZARD) / 10 + 1) * GET_LEVEL(mob); /* con bonus */
-    } else if (GET_SPECIALTY_SCHOOL(ch) == CONJURATION &&
+    } else if (GET_SPECIALTY_SCHOOL(ch) == NECROMANCY &&
         ( spellnum == SPELL_ANIMATE_DEAD ||
           spellnum == SPELL_GREATER_ANIMATION ||
           spellnum == SPELL_MUMMY_DUST )
