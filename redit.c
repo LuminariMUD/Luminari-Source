c068d268a6569f19567083b126649a2484d04533
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2015-06-01 08:50:40 +0000

    adding some unimplemented feats, sorting of feats in the code, and some minor bug fixes
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@2681 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- redit.c
+++ redit.c
@@ -91,11 +91,13 @@ ACMD(do_oasis_redit)
   }
 
   /* dummy check, we can't redit the dynamic vnums in wilderness */
+  /*
   if (real_room(number) != NOWHERE && IS_DYNAMIC(real_room(number))) {
     send_to_char(ch, "Dynamic Wilderness rooms cannot be editted, please use "
             "buildwalk, then redit those rooms created with buildwalk.\r\n");
     return;
   }
+  */
 
   /* Retrieve the player's descriptor. */
   d = ch->desc;
