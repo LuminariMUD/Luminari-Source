23cfa8b130b5887c33cdfc93ec00234d8b9cedec
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2015-06-26 09:49:42 +0000

    added debug to move routines...
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@2735 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- handler.c
+++ handler.c
@@ -950,7 +950,11 @@ void char_to_room(struct char_data *ch, room_rnum room) {
 
       }
       /* Set occupied flag */
+      
+      log("room: %d real_room(room): %d is_dynamic(room) %d\n", room, real_room(room), IS_DYNAMIC(room));
+      
       if (real_room(room) != NOWHERE &&  IS_DYNAMIC(room) ) {
+        log("Setting occupied bit to room: %d", room);
         SET_BIT_AR(ROOM_FLAGS(room), ROOM_OCCUPIED);
         /* Create the event to clear the flag, if it is not already set. */
         if(!room_has_mud_event(&world[room], eCHECK_OCCUPIED))
