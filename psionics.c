28daae8bf3f8e0960ea8bb96f701858b31ca64ce
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2015-11-12 21:28:35 +0000

    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@2799 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

---
+++ psionics.c
@@ -0,0 +1,41 @@
+/**************************************************************************
+ *  File: psionics.c                                   Part of LuminariMUD *
+ *  Usage: Psionic abilities                                               *
+ *  Author: Zusuk                                                                       *
+ **************************************************************************/
+
+#include <zconf.h>
+
+#include "conf.h"
+#include "sysdep.h"
+#include "structs.h"
+#include "utils.h"
+#include "comm.h"
+#include "interpreter.h"
+#include "handler.h"
+#include "db.h"
+
+/*
+#include "spells.h"
+#include "act.h"
+#include "fight.h"
+#include "mud_event.h"
+#include "constants.h"
+#include "spec_procs.h"
+#include "class.h"
+#include "mudlim.h"
+#include "actions.h"
+#include "actionqueues.h"
+#include "assign_wpn_armor.h"
+#include "feats.h"
+*/
+
+/* GLOBALS */
+
+/* UTILITY FUNCTIONS */
+
+/* PRIMARY FUNCTIONS */
+
+
+
+/* EOF */
