c0a0fed8cde87f085bda4ca8f41bb3ff93838ec2
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2015-12-09 14:38:27 +0000

    All changes for new description engine.
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@2819 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- mysql.c
+++ mysql.c
@@ -380,14 +380,14 @@ struct region_proximity_list* get_nearby_regions(zone_rnum zone, int x, int y, i
     /* Allocate memory for the region data. */
     CREATE(new_node, struct region_proximity_list, 1);
     new_node->rnum = real_region(atoi(row[0]));    
-    new_node->n  = atoi(row[1]));
-    new_node->ne = atoi(row[2]));    
-    new_node->e  = atoi(row[3]));
-    new_node->se = atoi(row[4]));
-    new_node->s  = atoi(row[5]));
-    new_node->sw = atoi(row[6]));
-    new_node->w  = atoi(row[7]));
-    new_node->nw = atoi(row[8]));   
+    new_node->n  = atoi(row[1]);
+    new_node->ne = atoi(row[2]);    
+    new_node->e  = atoi(row[3]);
+    new_node->se = atoi(row[4]);
+    new_node->s  = atoi(row[5]);
+    new_node->sw = atoi(row[6]);
+    new_node->w  = atoi(row[7]);
+    new_node->nw = atoi(row[8]);   
     new_node->next = regions;
     regions = new_node;
     new_node = NULL; 
