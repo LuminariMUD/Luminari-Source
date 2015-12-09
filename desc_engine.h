0a4c19df4bc0019ae80ff5444b8b987ee1a4b022
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2015-12-09 11:49:07 +0000

    All changes for new description engine.
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@2803 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

---
+++ desc_engine.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   desc_engine.h
+ * Author: jamclaug
+ *
+ * Created on 9. desember 2015, 12:48
+ */
+
+#ifndef DESC_ENGINE_H
+#define	DESC_ENGINE_H
+
+char * gen_room_description(struct char_data *ch, struct room_data *rm);  
+
+
+#endif	/* DESC_ENGINE_H */
+
