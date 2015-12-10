dba82657c3b2e6ed17055c7c701a80e3ed7d90b4
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2015-12-10 11:40:04 +0000

    All changes for new description engine.
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@2833 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- mysql.c
+++ mysql.c
@@ -349,7 +349,12 @@ struct region_proximity_list* get_nearby_regions(zone_rnum zone, int x, int y, i
                "    when ST_Intersects(ri.region_polygon, "
                "                       geomfromtext('polygon((%d %d, %f %f, %f %f, %d %d))')) "
                "    then 1 else 0 end as nw "    
-               "  from region_index as ri) nearby_regions "
+               "  from region_index as ri, "
+               "       region_data as rd "
+               "  where ri.vnum = rd.vnum and"
+               "        rd.region_type = 1 "
+               "  order by ST_Distance(ri.region_polygon, Point(%d, %d) desc " // GEROGRAPHIC regions only.
+               " ) nearby_regions "
                "  where ((n = 1) or (ne = 1) or (e = 1) or (se = 1) or (s = 1) or (sw = 1) or (w = 1) or (nw = 1));",                
                x, y, (r*-.5 + x), (r*.87 + y), (r*.5 + x), (r*.87 + y), x, y,    /* n */
                x, y, (r*.5 + x),  (r*.87 + y), (r*.87 + x), (r*.5 + y), x, y,    /* ne */
