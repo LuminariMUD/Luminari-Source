b34dbd48de08aef4cc052dd870071a90ead1cd18
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2015-12-11 12:50:09 +0000

    All changes for new description engine.
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@2865 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- desc_engine.c
+++ desc_engine.c
@@ -184,7 +184,7 @@ char * gen_room_description(struct char_data *ch, room_rnum room) {
     if (curr_nearby_region->ne) {
       if (curr_nearby_region->ne > max_area) {
         max_area = curr_nearby_region->ne;
-        region_dir = 1;
+        region_dir = 2;
       }      
       sprintf(buf, "%s lies %sto the northeast.\r\n", region_table[curr_nearby_region->rnum].name,
               (curr_nearby_region->dist <= 1 ? "very near " : 
@@ -199,7 +199,7 @@ char * gen_room_description(struct char_data *ch, room_rnum room) {
     if (curr_nearby_region->e) {
       if (curr_nearby_region->e > max_area) {
         max_area = curr_nearby_region->e;
-        region_dir = 1;
+        region_dir = 3;
       }      
       sprintf(buf, "%s lies %sto the east.\r\n", region_table[curr_nearby_region->rnum].name,
               (curr_nearby_region->dist <= 1 ? "very near " : 
@@ -214,7 +214,7 @@ char * gen_room_description(struct char_data *ch, room_rnum room) {
     if (curr_nearby_region->se) {
       if (curr_nearby_region->se > max_area) {
         max_area = curr_nearby_region->se;
-        region_dir = 1;
+        region_dir = 4;
       }
       sprintf(buf, "%s lies %sto the southeast.\r\n", region_table[curr_nearby_region->rnum].name,
               (curr_nearby_region->dist <= 1 ? "very near " : 
@@ -229,7 +229,7 @@ char * gen_room_description(struct char_data *ch, room_rnum room) {
     if (curr_nearby_region->s) {
       if (curr_nearby_region->s > max_area) {
         max_area = curr_nearby_region->s;
-        region_dir = 1;
+        region_dir = 5;
       }      
       sprintf(buf, "%s lies %sto the south.\r\n", region_table[curr_nearby_region->rnum].name,
               (curr_nearby_region->dist <= 1 ? "very near " : 
@@ -244,7 +244,7 @@ char * gen_room_description(struct char_data *ch, room_rnum room) {
     if (curr_nearby_region->sw) {
       if (curr_nearby_region->sw > max_area) {
         max_area = curr_nearby_region->sw;
-        region_dir = 1;
+        region_dir = 6;
       }      
       sprintf(buf, "%s lies %sto the southwest.\r\n", region_table[curr_nearby_region->rnum].name,
               (curr_nearby_region->dist <= 1 ? "very near " : 
@@ -259,7 +259,7 @@ char * gen_room_description(struct char_data *ch, room_rnum room) {
     if (curr_nearby_region->w) {
       if (curr_nearby_region->w > max_area) {
         max_area = curr_nearby_region->w;
-        region_dir = 1;
+        region_dir = 7;
       }      
       sprintf(buf, "%s lies %sto the west.\r\n", region_table[curr_nearby_region->rnum].name,
               (curr_nearby_region->dist <= 1 ? "very near " : 
@@ -274,7 +274,7 @@ char * gen_room_description(struct char_data *ch, room_rnum room) {
     if (curr_nearby_region->nw) {
       if (curr_nearby_region->nw > max_area) {
         max_area = curr_nearby_region->nw;
-        region_dir = 1;
+        region_dir = 8;
       }      
       sprintf(buf, "%s lies %sto the northwest.\r\n", region_table[curr_nearby_region->rnum].name,
               (curr_nearby_region->dist <= 1 ? "very near " : 
@@ -288,7 +288,7 @@ char * gen_room_description(struct char_data *ch, room_rnum room) {
     }
   }  
   
-  log("max_area : %f region_dir : %d", max_area, region_dir);
+  log("max_area : %f region_dir : %s", max_area, direction_strings[region_dir]);
   
   if (rdesc != NULL)
     return strdup(rdesc); 
