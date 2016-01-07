18cb269b0c762fd48eb8821ffa6486afe82f297f
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2016-01-07 09:49:10 +0000

    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@2977 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- objsave.c
+++ objsave.c
@@ -376,6 +376,7 @@ int Crash_delete_file(char *name) {
     log("SYSERR: deleting crash file %s (2): %s", filename, strerror(errno));
 
   /* we have successfully deleted the crash-save-file */
+  log("debug: deleting crash file %s (2): %s", filename);
   return TRUE;
 }
 
