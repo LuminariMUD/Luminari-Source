cf2a570dc8ff3cb090e81e6541bd0acd2574186d
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2016-01-07 19:44:39 +0000

    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@2996 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- bardic_performance.h
+++ bardic_performance.h
@@ -12,14 +12,39 @@
 extern "C" {
 #endif
 /*********************************************************/
-    
+/* includes */
+#include "utils.h" /* for the ACMD macro */
+
 /* defines */
 #define VERSE_INTERVAL       (7 RL_SEC)
 #define MAX_PERFORMANCES     12
-    
-#define PERFORMANCE_SKILLNUM 0
-#define INSTRUMENT_NUM       1
-#define INSTRUMENT_SKILLNUM  2
+/* lookup components for song_info */    
+#define PERFORMANCE_SKILLNUM             0
+#define INSTRUMENT_NUM                   1
+#define INSTRUMENT_SKILLNUM              2
+#define PERFORMANCE_DIFF                 3
+#define PERFORMANCE_TYPE                 4
+/*last*/#define PERFORMANCE_INFO_FIELDS  5
+/* types of performances */
+#define PERFORMANCE_TYPE_UNDEFINED      0
+#define PERFORMANCE_TYPE_ACT            1
+#define PERFORMANCE_TYPE_COMEDY         2
+#define PERFORMANCE_TYPE_DANCE          3
+#define PERFORMANCE_TYPE_KEYBOARD       4
+#define PERFORMANCE_TYPE_ORATORY        5
+#define PERFORMANCE_TYPE_PERCUSSION     6
+#define PERFORMANCE_TYPE_STRING         7
+#define PERFORMANCE_TYPE_WIND           8
+#define PERFORMANCE_TYPE_SING           9
+/*last*/#define NUM_PERFORMANCE_TYPES  10
+/* these are defines just made for fillers for lookup data song_info
+ since they are currently unused in our feat system, could be expanded tho */
+#define SKILL_LYRE     1
+#define SKILL_DRUM     2
+#define SKILL_HORN     3
+#define SKILL_FLUTE    4
+#define SKILL_HARP     5
+#define SKILL_MANDOLIN 6
 
 /* functions */
 extern struct room_data *world;
