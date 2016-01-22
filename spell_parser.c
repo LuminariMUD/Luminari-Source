2fbe6cfc90dcd4b07e9a4eea946fcb602445d377
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2016-01-22 09:29:50 +0000

    Metamagic
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@3260 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- spell_parser.c
+++ spell_parser.c
@@ -1232,8 +1232,15 @@ int cast_spell(struct char_data *ch, struct char_data *tch,
 
   casting_time = SINFO.time;
   
-  if (IS_SET(metamagic, METAMAGIC_QUICKEN)) {
+  if (class != CLASS_SORCERER && 
+      class != CLASS_BARD && 
+      IS_SET(metamagic, METAMAGIC_QUICKEN)) {
     casting_time /= 2;
+  }
+  if ((class == CLASS_SORCERER || class = CLASS_BARD) && 
+      IS_SET(metamagic, METAMAGIC_MAXIMIZE) &&
+      !IS_SET(metamagic, METAMAGIC_QUICKEN)) {
+    casting_time *= 2;
   }
   
   /* handle spells with no casting time */
