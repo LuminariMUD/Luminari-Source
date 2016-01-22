a3ad091c35ae80c1b543c44a98bdb99ce1123b6b
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2016-01-22 09:49:12 +0000

    Metamagic
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@3266 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- spell_parser.c
+++ spell_parser.c
@@ -1039,10 +1039,7 @@ EVENTFUNC(event_casting) {
 
       if (!concentration_check(ch, spellnum))
         return 0;
-
-                       (IS_SET(CASTING_METAMAGIC(ch), METAMAGIC_QUICKEN) ? "quickened ": ""),
-                       (IS_SET(CASTING_METAMAGIC(ch), METAMAGIC_MAXIMIZE) ? "maximized ": ""),
-                       spell_info[PREPARATION_QUEUE(ch, 0, classArray(class)).spell].name);
+                  
       //display time left to finish spell
       sprintf(buf, "Casting: %s%s%s ",
               (IS_SET(CASTING_METAMAGIC(ch), METAMAGIC_QUICKEN) ? "quickened ": ""),
