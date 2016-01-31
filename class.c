d1eaee8adde7e1db7b13e832de37898f2ea4d3e4
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2016-01-31 20:06:15 +0000

    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@3366 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- class.c
+++ class.c
@@ -1815,7 +1815,7 @@ bool display_class_info(struct char_data *ch, char *classname) {
   send_to_char(ch, "\tC");
   draw_line(ch, line_length, '-', '-');
   
-  send_to_char(ch, "@YType: @Rclassfeat %s@Y for the class feat info.\r\n",
+  send_to_char(ch, "\tYType: \tRclassfeat %s\tY for the class feat info.\tn\r\n",
     CLSLIST_NAME(class));
  
   send_to_char(ch, "\tC");
