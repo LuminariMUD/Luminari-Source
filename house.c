ee6478e746b67d1fd5db987fae65568d5025927d
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2016-02-09 13:29:05 +0000

    removed shopkeepers puking on customers, that is just unhygenic.
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@3451 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- house.c
+++ house.c
@@ -111,8 +111,7 @@ static int handle_house_obj(struct obj_data *temp, room_vnum vnum,  int locate, 
       obj_to_room(temp, rnum); /* add to room first ... */
       log ("adding obj to room 4...");
     } else { /* object isn't container -> empty content list */
-      
-      
+            
       for (; cont_row[j]; cont_row[j] = obj1) {
         obj1 = cont_row[j]->next_content;
         obj_to_room(cont_row[j], rnum);
