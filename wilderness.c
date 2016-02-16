00c30720e0a60aec7c57f8a59213ef9f78b5e8ea
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2016-02-16 12:52:55 +0000

    Weather, first pass
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@3525 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- wilderness.c
+++ wilderness.c
@@ -221,9 +221,9 @@ int get_weather(int x, int y) {
   trans_x = x / (double) (WILD_X_SIZE / 2.0);
   trans_y = y / (double) (WILD_Y_SIZE / 2.0);
 
-  result = PerlinNoise3D(NOISE_WEATHER, trans_x, trans_y, time_base, 2, 2.0, 1);
+  result = PerlinNoise3D(NOISE_WEATHER, trans_x, trans_y, time_base, 2.0, 2.0, 8);
   
-  log("DEBUG: Weather - %f %f %d %f", trans_x, trans_y, time_base, result);
+  //log("DEBUG: Weather - %f %f %d %f", trans_x, trans_y, time_base, result);
   
   result = (result + 1) / 2.0;
   
