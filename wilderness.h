f607ff3a692a0c77aaf4019e51e70ea4cf1d1708
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2016-02-17 13:56:30 +0000

    Weather, first pass
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@3557 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- wilderness.h
+++ wilderness.h
@@ -191,6 +191,7 @@ struct wild_map_tile {
 
 void get_map(int xsize, int ysize, int center_x, int center_y, struct wild_map_tile **map);
 int get_sector_type(int elevation, int temperature, int moisture);
+int get_weather(int x, int y);
 void show_wilderness_map(struct char_data *ch, int size, int x, int y);
 void save_map_to_file(const char *fn, int xsize, int ysize);
 void save_noise_to_file(int idx, const char* fn, int xsize, int ysize, int zoom);
