468608a1a6df36196e4e4c058cd8493db1f038df
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2016-09-15 09:28:06 +0000

    bug fixes
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@3731 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- interpreter.c
+++ interpreter.c
@@ -711,6 +711,10 @@ void command_interpreter(struct char_data *ch, char *argument) {
        will call the same command in the code, but are sending a different ARG to
        the command trigger checks...  resolution for now is to just replace the
        "'" with "say" when we find it here. -Zusuk */
+    if (arg[0] == '\'') {
+      char saystring[3] = "say";
+      *arg = *saystring;
+    }
     
     int cont; /* continue the command checks */
     cont = command_wtrigger(ch, arg, line); /* any world triggers ? */
