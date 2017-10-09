5a1cdc8d5072a4f8a49e80af17f25397d46557bf
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2017-10-09 14:16:09 +0000

    bug fixes
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@3748 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- spec_procs.c
+++ spec_procs.c
@@ -6207,9 +6207,9 @@ SPECIAL(bank) {
 
   if (CMD_IS("balance")) {
     if (GET_BANK_GOLD(ch) > 0)
-      send_to_char(ch, "Your current balance is %d coins.\r\n", GET_BANK_GOLD(ch));
+      send_to_char(ch, "@wYour current balance is @W%d @Ycoins@w.@n\r\n", GET_BANK_GOLD(ch));
     else
-      send_to_char(ch, "You currently have no money deposited.\r\n");
+      send_to_char(ch, "@wYou currently have @Wno@w money deposited.@n\r\n");
     return (TRUE);
   } else if (CMD_IS("deposit")) {
     
@@ -6217,7 +6217,7 @@ SPECIAL(bank) {
     skip_spaces(&argument);
     if (is_abbrev(argument, "all")) {
       amount = GET_GOLD(ch);
-      send_to_char(ch, "You deposit all (%d) your coins.\r\n", amount);
+      send_to_char(ch, "@wYou deposit all (@W%d@w) your @Ycoins@w.@n\r\n", amount);
       act("$n makes a bank transaction.", TRUE, ch, 0, FALSE, TO_ROOM);
       decrease_gold(ch, amount);
       increase_bank(ch, amount);
@@ -6234,7 +6234,7 @@ SPECIAL(bank) {
     }
     decrease_gold(ch, amount);
     increase_bank(ch, amount);
-    send_to_char(ch, "You deposit %d coins.\r\n", amount);
+    send_to_char(ch, "@wYou deposit @W%d@Y coins@w.@n\r\n", amount);
     act("$n makes a bank transaction.", TRUE, ch, 0, FALSE, TO_ROOM);
     return (TRUE);
   } else if (CMD_IS("withdraw")) {
@@ -6243,7 +6243,7 @@ SPECIAL(bank) {
     skip_spaces(&argument);
     if (is_abbrev(argument, "all")) {
       amount = GET_BANK_GOLD(ch);
-      send_to_char(ch, "You withdraw all (%d) your coins.\r\n", amount);
+      send_to_char(ch, "@wYou withdraw all (@W%d@w) your @Ycoins@w.@n\r\n", amount);
       act("$n makes a bank transaction.", TRUE, ch, 0, FALSE, TO_ROOM);
       increase_gold(ch, amount);
       decrease_bank(ch, amount);
@@ -6260,7 +6260,7 @@ SPECIAL(bank) {
     }
     increase_gold(ch, amount);
     decrease_bank(ch, amount);
-    send_to_char(ch, "You withdraw %d coins.\r\n", amount);
+    send_to_char(ch, "@wYou withdraw @W%d @Ycoins@w.@n\r\n", amount);
     act("$n makes a bank transaction.", TRUE, ch, 0, FALSE, TO_ROOM);
     return (TRUE);
   } else
