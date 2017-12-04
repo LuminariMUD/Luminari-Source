8c1b4244884fec6b05ecf1c5613198927e410e90
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2017-12-04 18:43:12 +0000

    ...
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@3899 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- act.other.c
+++ act.other.c
@@ -3520,7 +3520,7 @@ void update_msdp_group(struct char_data *ch) {
   
   msdp_buffer[0] = '\0';
   if (ch && ch->desc) {
-    while ((k = (struct char_data *) simple_list(ch->group->members)) != NULL)    
+    while ((k = (struct char_data *) simple_list(ch->group->members)) != NULL) {   
       char buf[4000]; // Buffer for building the group table for MSDP    
       next = af->next;
       sprintf(buf, "%c%c"
