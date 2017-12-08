eae30e40ea68f6298896027a560e84479304444a
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2017-12-08 08:22:41 +0000

    Reordered help search to put classes before the help system
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@3946 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- help.c
+++ help.c
@@ -234,10 +234,9 @@ ACMD(do_help) {
   raw_argument = strdup(argument);
   space_to_minus(argument);
 
-  if ((entries = search_help(argument, GET_LEVEL(ch))) == NULL) {
+  if (!display_class_info(ch, raw_argument) && (entries = search_help(argument, GET_LEVEL(ch))) == NULL) {
     /* Check feats for relevant entries! */
-    if (!display_feat_info(ch, raw_argument) &&
-            !display_class_info(ch, raw_argument)) {
+    if (!display_feat_info(ch, raw_argument)) {
     
       send_to_char(ch, "There is no help on that word.\r\n");
       mudlog(NRM, MAX(LVL_IMPL, GET_INVIS_LEV(ch)), TRUE,
