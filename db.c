dda72e058f4fbe6ad800677be5f5f5801a8d031f
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2018-01-07 07:14:33 +0000

    bug fixes
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@4143 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- db.c
+++ db.c
@@ -935,7 +935,7 @@ void boot_db(void) {
 
   for (i = 0; i <= top_of_zone_table; i++) {
     buf1 = zone_table[i].name;
-    strip_colors(buf);
+    strip_colors(buf1);
     log("Resetting #%d: %s (rooms %d-%d).", zone_table[i].number,
             buf, zone_table[i].bot, zone_table[i].top);
     reset_zone(i);
