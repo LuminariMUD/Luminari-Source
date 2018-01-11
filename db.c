a5929936c5ce1afd90e939b3f451b271fc57d554
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2018-01-11 20:43:33 +0000

    code fixes
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@4254 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- db.c
+++ db.c
@@ -4075,6 +4075,12 @@ void free_char(struct char_data *ch) {
       GET_ALIASES(ch) = (GET_ALIASES(ch))->next;
       free_alias(a);
     }
+    
+    /* Free the action queue */
+    if (GET_QUEUE(ch))
+      free_action_queue(GET_QUEUE(ch));
+    if (GET_ATTACK_QUEUE(ch))
+      free_attack_queue(GET_ATTACK_QUEUE(ch));
 
     if (ch->player_specials->poofin)
       free(ch->player_specials->poofin);
@@ -4154,11 +4160,6 @@ void free_char(struct char_data *ch) {
   if (SCRIPT(ch))
     extract_script(ch, MOB_TRIGGER);
 
-  /* Free the action queue */
-  if (GET_QUEUE(ch))
-    free_action_queue(GET_QUEUE(ch));
-  if (GET_ATTACK_QUEUE(ch))
-    free_attack_queue(GET_ATTACK_QUEUE(ch));
   
   /* Mud Events */
   if (ch->events != NULL) {
