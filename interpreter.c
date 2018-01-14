caa7caf207910036b674b7b9907fd86b83e6f773
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2018-01-14 14:59:03 +0000

    disabled memtest command
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@4317 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- interpreter.c
+++ interpreter.c
@@ -201,6 +201,7 @@ cpp_extern const struct command_info cmd_info[] = {
   { "copycat", "copycat", POS_FIGHTING, do_copycat, 1, 0, FALSE, ACTION_STANDARD, {6, 0}},
   { "class", "class", POS_DEAD, do_class, 0, 0, TRUE, ACTION_NONE, {0, 0}},
   { "combatroll", "combatroll", POS_DEAD, do_gen_tog, 0, SCMD_COMBATROLL, TRUE, ACTION_NONE, {0, 0}},
+  { "coordconvert", "coordconvert", POS_SLEEPING, do_coordconvert, LVL_IMMORT, 0, TRUE, ACTION_NONE, {0, 0}},
 
   { "date", "da", POS_DEAD, do_date, 1, SCMD_DATE, TRUE, ACTION_NONE, {0, 0}},
   { "dc", "dc", POS_DEAD, do_dc, LVL_STAFF, 0, TRUE, ACTION_NONE, {0, 0}},
