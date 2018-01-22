957779e637fde2088a820ee3f3a0d2b8ad132615
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2018-01-22 19:20:46 +0000

    more work on spell prep
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@4540 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- spell_prep.c
+++ spell_prep.c
@@ -605,9 +605,11 @@ bool is_spell_in_collection(struct char_data *ch, int class, int spellnum,
    out: bool, is a known spell or not */
 bool is_a_known_spell(struct char_data *ch, int class, int spellnum) {
   /*DEBUG*/
-  if ((class == CLASS_SORCERER || class == CLASS_BARD) && GET_LEVEL(ch) >= LVL_IMMORT) {
-    send_to_char(ch, "Staff over-ride in is_a_known_spell()\r\n");
-    return TRUE;
+  if (GET_LEVEL(ch) >= LVL_IMMORT) {
+    if (class == CLASS_SORCERER || class == CLASS_BARD) {
+      send_to_char(ch, "Staff over-ride in is_a_known_spell()\r\n");
+      return TRUE;
+    }
   }
   /*DEBUG*/
   
