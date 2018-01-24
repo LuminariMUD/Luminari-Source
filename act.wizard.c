8ca02830aa03d80b65a494b8b2103d97d169bed4
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2018-01-24 16:19:20 +0000

    typos
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@4639 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- act.wizard.c
+++ act.wizard.c
@@ -4733,7 +4733,7 @@ EVENTFUNC(event_copyover) {
   /* current timer */
   if (initial_timer)
     current_timer = initial_timer; /* seconds */
-  else if (copyover_event->pEvent->q_el)
+  else if (copyover_event->pEvent)
     current_timer = (int) (event_time(copyover_event->pEvent) / 10); /* seconds */
   else
     current_timer = 0;
