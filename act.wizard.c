f676541bfeca923f3374961d12d4923ef1fe20af
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2018-01-24 14:58:45 +0000

    typos
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@4630 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- act.wizard.c
+++ act.wizard.c
@@ -4647,7 +4647,7 @@ void perform_do_copyover() {
       write_to_descriptor(d->descriptor, "\n\rSorry, we are rebooting. Come back in a few minutes.\n\r");
       close_socket(d); /* throw'em out */
     } else {
-      send_to_char(och, "\n\r \tR*** \tWTime stops for a moment as space and time folds upon itself! \tR***\tn\n\r"
+      write_to_descriptor(d->descriptor, "\n\r \tR*** \tWTime stops for a moment as space and time folds upon itself! \tR***\tn\n\r"
               "\tc[The game will pause for about 30 seconds while new code is being imported, "
               "you will need to reform if you were grouped.  If you get disconnected, "
               "you should be able to reconnect immediately or within a few minutes.]\tn\r\n");
