5ccca1cb99aa4e95f4db084c63cfde3706dca97a
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2018-01-30 13:47:30 +0000

    ...
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@4853 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- mysql.h
+++ mysql.h
@@ -20,6 +20,7 @@ struct wilderness_data* load_wilderness(zone_vnum zone);
 void load_regions();
 struct region_list* get_enclosing_regions(zone_rnum zone, int x, int y);
 void load_paths();
+void insert_path(struct path_data *path);
 struct path_list* get_enclosing_paths(zone_rnum zone, int x, int y);
 
 struct region_proximity_list* get_nearby_regions(zone_rnum zone, int x, int y, int r);
