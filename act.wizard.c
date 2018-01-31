0509771bc81796100ced9310f1c0658c79726bc4
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2018-01-31 10:21:57 +0000

    ...
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@4894 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- act.wizard.c
+++ act.wizard.c
@@ -6025,33 +6025,6 @@ ACMD(do_genmap) {
 
   generate_river(ch, dir, vnum, name);
   load_paths();
-
-  /*
-    void *set;
-    double pos[2], point[2];
-    room_rnum *room;
-    //  int j = 0;
-
-    point[0] = 0;
-    point[1] = 0;
-    set = kd_nearest_range(kd_wilderness_rooms, point, 2048);
-    send_to_char(ch, "range query returned %d items.\r\n", kd_res_size(set));
-
-    while (!kd_res_end(set)) {
-   */
-  /* get the data and position of the current result item */
-  /*
-    room = (room_rnum *) kd_res_item(set, pos);
-    send_to_char(ch, " (%.3f, %.3f) is room vnum: %d\r\n", pos[0], pos[1], world[*room].number);
-   */
-  /* go to the next entry */
-  /*
-      kd_res_next(set);
-    }
-
-    kd_res_free(set);
-   */
-  // Test region events
 }
 
 /* do_acconvert - Commant to convert exising armor to the new (Sept 9, 2014)
