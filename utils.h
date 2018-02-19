0fc8b33a72a11c31e6e6c27af38f16fa3b513e38
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2018-02-19 13:08:57 +0000

    ....
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@5119 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- utils.h
+++ utils.h
@@ -143,7 +143,7 @@ int get_levelup_sorcerer_bloodline_type(struct char_data *ch);
 int get_daily_uses(struct char_data *ch, int featnum);
 int start_daily_use_cooldown(struct char_data *ch, int featnum);
 int daily_uses_remaining(struct char_data *ch, int featnum);
-int daily_armor_specab_uses_remaining(struct obj_data *, int specab);
+int daily_armor_specab_uses_remaining(struct obj_data *obj, int specab);
 int start_armor_specab_daily_use_cooldown(struct obj_data *obj, int specab);
 
 /* ASCII output formatting */
