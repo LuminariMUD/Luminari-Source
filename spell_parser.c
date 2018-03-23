d9100afffc2774ec8f614913734356285905ee78
luminari <luminari@dda7f814-ccd3-4591-a6d7-1bd23e5e64af>
2018-03-23 14:28:40 +0000

    Added a better message when trying to cast a spell from a restricted school.
    git-svn-id: svn+ssh://50.28.74.132/home/luminari/svn/luminari/myproject/trunk@5205 dda7f814-ccd3-4591-a6d7-1bd23e5e64af

--- spell_parser.c
+++ spell_parser.c
@@ -1472,6 +1472,13 @@ ACMD(do_gen_cast) {
     return;
   }
   
+  /* 3.23.18 Ornir : Add better message when you try to cast a spell from a restricted school. */
+  if (spell_info[spellnum].schoolOfMagic ==
+            restricted_school_reference[GET_SPECIALTY_SCHOOL(ch)]) {
+    send_to_char(ch, "You are unable to cast spells from this school of magic.\r\n");
+    return;
+  }
+
   /* this is the block to make sure they meet the min-level reqs */
   if (isEpicSpell(spellnum)) {
     switch (spellnum) {
