40fd1aa372bc06ff48357bb88b20bd8478868aee
GickerLDS <gickerlds@gmail.com>
2020-04-24 23:25:16 +0100

    Added cha and appraise to weapon and armor vendor prices. Updated copy scripts to rename binary instead of removing it.

--- copybinarytolive.sh
+++ copybinarytolive.sh
@@ -1,4 +1,5 @@
-rm /home/luminari/mud/bin/circle
+TIMEDATE=$(date +"%m-%d-%Y-%H-%M")
+mv /home/luminari/mud/bin/circle /home/luminari/mud/bin/circle.$TIMEDATE
 cp ../bin/circle /home/luminari/mud/bin
 echo Copied binary to luminari live port
 cp /home/luminari/mud/changelog /home/luminari/mud/lib/text/news
