a4212bf85fa363cea38f927f3f20176efd2ccd00
GickerLDS <gickerlds@gmail.com>
2020-04-24 23:25:16 +0100

    Added cha and appraise to weapon and armor vendor prices. Updated copy scripts to rename binary instead of removing it.

--- copybinarytodev.sh
+++ copybinarytodev.sh
@@ -1,3 +1,4 @@
-rm /home/luminari/dev/bin/circle
+TIMEDATE=$(date +"%m-%d-%Y-%H-%M")
+mv /home/luminari/dev/bin/circle /home/luminari/dev/bin/circle.$TIMEDATE
 cp ../bin/circle /home/luminari/dev/bin
 echo Copied binary to luminari dev port
