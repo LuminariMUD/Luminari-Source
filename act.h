e45ba494d541cebc50f2587800bde8d2e2a7fdd9
GickerLDS <gickerlds@gmail.com>
2020-04-29 11:48:02 -0300

    Added needed auction defnes

--- act.h
+++ act.h
@@ -303,6 +303,24 @@ ACMD_DECL(do_eat);
 ACMD_DECL(do_pour);
 #define SCMD_POUR 0
 #define SCMD_FILL 1
+
+ /* AUCTIONING STATES */
+#define AUC_NULL_STATE          0   /* not doing anything */
+#define AUC_OFFERING            1   /* object has been offfered */
+#define AUC_GOING_ONCE          2       /* object is going once! */
+#define AUC_GOING_TWICE         3       /* object is going twice! */
+#define AUC_LAST_CALL           4       /* last call for the object! */
+#define AUC_SOLD                5
+
+/* AUCTION CANCEL STATES */
+#define AUC_NORMAL_CANCEL       6       /* normal cancellation of auction */
+#define AUC_QUIT_CANCEL         7       /* auction canclled because player quit */
+#define AUC_WIZ_CANCEL          8       /* auction cancelled by a god */
+
+/* OTHER JUNK */
+#define AUC_STAT                9
+#define AUC_BID                 10
+
 /* functions without subcommands */
 ACMD_DECL(do_drink);
 ACMD_DECL(do_get);
