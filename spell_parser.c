6a764ae601cea128f0991f527ad14c6464200e0f
GickerLDS <gickerlds@gmail.com>
2020-05-01 12:04:28 -0300

    Added additional code enabling the use of the mission system.

--- spell_parser.c
+++ spell_parser.c
@@ -464,7 +464,7 @@ int call_magic(struct char_data *caster, struct char_data *cvict,
                 return (0);
         }
 
-        if (cvict && MOB_FLAGGED(cvict, MOB_NOKILL))
+        if (cvict && (MOB_FLAGGED(cvict, MOB_NOKILL) || !is_mission_mob(caster, cvict)))
         {
                 send_to_char(caster, "This mob is protected.\r\n");
                 return (0);
