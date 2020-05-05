b28d2954719fc34888e8ff5aeb7448d5eb64bff0
Mosheh Ben Avraham <moshehwebservices@live.com>
2020-05-05 07:53:07 +0300

    happyhour token
    happyhour token

--- act.h
+++ act.h
@@ -304,22 +304,22 @@ ACMD_DECL(do_pour);
 #define SCMD_POUR 0
 #define SCMD_FILL 1
 
- /* AUCTIONING STATES */
-#define AUC_NULL_STATE          0   /* not doing anything */
-#define AUC_OFFERING            1   /* object has been offfered */
-#define AUC_GOING_ONCE          2       /* object is going once! */
-#define AUC_GOING_TWICE         3       /* object is going twice! */
-#define AUC_LAST_CALL           4       /* last call for the object! */
-#define AUC_SOLD                5
+/* AUCTIONING STATES */
+#define AUC_NULL_STATE 0  /* not doing anything */
+#define AUC_OFFERING 1    /* object has been offfered */
+#define AUC_GOING_ONCE 2  /* object is going once! */
+#define AUC_GOING_TWICE 3 /* object is going twice! */
+#define AUC_LAST_CALL 4   /* last call for the object! */
+#define AUC_SOLD 5
 
 /* AUCTION CANCEL STATES */
-#define AUC_NORMAL_CANCEL       6       /* normal cancellation of auction */
-#define AUC_QUIT_CANCEL         7       /* auction canclled because player quit */
-#define AUC_WIZ_CANCEL          8       /* auction cancelled by a god */
+#define AUC_NORMAL_CANCEL 6 /* normal cancellation of auction */
+#define AUC_QUIT_CANCEL 7   /* auction canclled because player quit */
+#define AUC_WIZ_CANCEL 8    /* auction cancelled by a god */
 
 /* OTHER JUNK */
-#define AUC_STAT                9
-#define AUC_BID                 10
+#define AUC_STAT 9
+#define AUC_BID 10
 
 /* functions without subcommands */
 ACMD_DECL(do_drink);
@@ -371,7 +371,7 @@ ACMD_DECL(do_pullswitch);
 #define SWITCH_OPEN 2
 /* Global variables from act.movement.c */
 #ifndef __ACT_MOVEMENT_C__
-extern const char * const cmd_door[];
+extern const char *const cmd_door[];
 #endif /* __ACT_MOVEMENT_C__ */
 
 /*****************************************************************************
@@ -520,6 +520,7 @@ ACMDCHECK(can_impromptu);
  * Begin Functions and defines for act.other.c
  ****************************************************************************/
 /* Functions with subcommands */
+void invoke_happyhour(struct char_data *ch);
 bool is_prompt_empty(struct char_data *ch);
 void set_bonus_attributes(struct char_data *ch, int str, int con, int dex, int ac);
 void list_forms(struct char_data *ch);
