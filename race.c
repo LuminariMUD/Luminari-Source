414c737597aa34e50f2ef90b429c08e27f950821
Mosheh Ben Avraham <moshehwebservices@live.com>
2020-08-22 17:12:33 +0300

    paladins use cha instead of wis for casting
    paladins use cha instead of wis for casting

--- race.c
+++ race.c
@@ -884,13 +884,13 @@ void assign_races(void)
   set_race_details(RACE_DUERGAR,
                    /*descrip*/ "Duergar dwell in subterranean caverns far from the touch of light. They detest all races "
                                "living beneath the sun, but that hatred pales beside their loathing of their surface-dwarf "
-		                  	       "cousins. Dwarves and duergar once were one race, but the dwarves left the deeps for their "
-                               "mountain strongholds. Duergar still consider themselves the only true dwarves, and the "
-                               "rightful heirs of all beneath the world’s surface. In appearance, duergar resemble gray-"
-                               "skinned dwarves, bearded but bald, with cold, lightless eyes. They favor taking captives "
+                               "cousins. Dwarves and Duergar once were one race, but the dwarves left the deeps for their "
+                               "mountain strongholds. Duergar still consider themselves the only true Dwarves, and the "
+                               "rightful heirs of all beneath the world’s surface. In appearance, Duergar resemble gray-"
+                               "skinned Dwarves, bearded but bald, with cold, lightless eyes. They favor taking captives "
                                "in battle over wanton slaughter, save for surface dwarves, who are slain without hesitation. "
                                "Duergar view life as ceaseless toil ended only by death. Though few can be described as "
-                               "anything other than vile and cruel, duergar still value honor and rarely break their word.",
+                               "anything other than vile and cruel, Duergar still value honor and rarely break their word.",
                    /*morph to-char*/ "Your body twists and contorts painfully until your form becomes Duergar.",
                    /*morph to-room*/ "$n's body twists and contorts painfully until $s form becomes Duergar.");
   set_race_genders(RACE_DUERGAR, N, Y, Y);                      /* n m f */
