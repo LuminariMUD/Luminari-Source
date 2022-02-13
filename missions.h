0853881f512d5032b3af1ba22b4b694d1e101003
Mosheh Ben Avraham <moshehwebservices@live.com>
2022-02-13 10:27:07 +0200

    formatting & replacing constant defines

--- missions.h
+++ missions.h
@@ -2,48 +2,50 @@
 #define _D20SW_MISSIONS_HPP_
 
 // defines
-#define MISSION_LVL             0
-#define MISSION_REBELS          1
-#define MISSION_EMPIRE          2
-#define MISSION_HUTTS           3
-#define MISSION_FREELANCERS     4
-#define MISSION_PLANET          5
-#define MISSION_ROOM_VNUM       6
-#define MISSION_ZONE_NAME       7
+#define MISSION_LVL 0
+#define MISSION_REBELS 1
+#define MISSION_EMPIRE 2
+#define MISSION_HUTTS 3
+#define MISSION_FREELANCERS 4
+#define MISSION_PLANET 5
+#define MISSION_ROOM_VNUM 6
+#define MISSION_ZONE_NAME 7
 
-#define MISSION_STANDING	1
-#define MISSION_REP		2
-#define MISSION_CREDITS		3
-#define MISSION_EXP		4
+#define MISSION_STANDING 1
+#define MISSION_REP 2
+#define MISSION_CREDITS 3
+#define MISSION_EXP 4
 
-#define MISSION_DIFF_EASY		0
-#define MISSION_DIFF_NORMAL		1
-#define MISSION_DIFF_TOUGH		2
-#define MISSION_DIFF_CHALLENGING	3
-#define MISSION_DIFF_ARDUOUS		4
-#define MISSION_DIFF_SEVERE		5
+#define MISSION_DIFF_EASY 0
+#define MISSION_DIFF_NORMAL 1
+#define MISSION_DIFF_TOUGH 2
+#define MISSION_DIFF_CHALLENGING 3
+#define MISSION_DIFF_ARDUOUS 4
+#define MISSION_DIFF_SEVERE 5
 
-#define NUM_MISSION_DIFFICULTIES        6
+#define NUM_MISSION_DIFFICULTIES 6
+#define MISSION_DETAIL_FIELDS 8
+#define MISSION_TARGET_FIELDS 5
 
-#define GET_CURRENT_MISSION(ch)			(ch->player_specials->saved.current_mission)
-#define GET_MISSION_CREDITS(ch)			(ch->player_specials->saved.mission_credits)
-#define GET_MISSION_STANDING(ch)		(ch->player_specials->saved.mission_standing)
-#define GET_MISSION_FACTION(ch)			(ch->player_specials->saved.mission_faction)
-#define GET_MISSION_REP(ch)			(ch->player_specials->saved.mission_reputation)
-#define GET_MISSION_EXP(ch)			(ch->player_specials->saved.mission_experience)
-#define GET_MISSION_DIFFICULTY(ch)		(ch->player_specials->saved.mission_difficulty)
-#define GET_MISSION_NPC_NAME_NUM(ch)		(ch->player_specials->saved.mission_rand_name)
-#define GET_MISSION_DECLINE(ch)                 (ch->player_specials->saved.mission_decline)
-#define GET_MISSION_COOLDOWN(ch)                 (ch->player_specials->saved.mission_cooldown)
-#define GET_MISSION_COMPLETE(ch)                (ch->player_specials->saved.mission_complete)
-#define GET_FACTION_STANDING(ch, i)             (ch->player_specials->saved.faction_standing[i])
+#define GET_CURRENT_MISSION(ch) (ch->player_specials->saved.current_mission)
+#define GET_MISSION_CREDITS(ch) (ch->player_specials->saved.mission_credits)
+#define GET_MISSION_STANDING(ch) (ch->player_specials->saved.mission_standing)
+#define GET_MISSION_FACTION(ch) (ch->player_specials->saved.mission_faction)
+#define GET_MISSION_REP(ch) (ch->player_specials->saved.mission_reputation)
+#define GET_MISSION_EXP(ch) (ch->player_specials->saved.mission_experience)
+#define GET_MISSION_DIFFICULTY(ch) (ch->player_specials->saved.mission_difficulty)
+#define GET_MISSION_NPC_NAME_NUM(ch) (ch->player_specials->saved.mission_rand_name)
+#define GET_MISSION_DECLINE(ch) (ch->player_specials->saved.mission_decline)
+#define GET_MISSION_COOLDOWN(ch) (ch->player_specials->saved.mission_cooldown)
+#define GET_MISSION_COMPLETE(ch) (ch->player_specials->saved.mission_complete)
+#define GET_FACTION_STANDING(ch, i) (ch->player_specials->saved.faction_standing[i])
 
 #define MISSION_MOB_DFLT_VNUM 60000
 
 // variables
-extern const char * const mission_details[][8];
-extern const char * const mission_targets[5];
-extern const char * const mission_difficulty[NUM_MISSION_DIFFICULTIES];
+extern const char *const mission_details[][MISSION_DETAIL_FIELDS];
+extern const char *const mission_targets[MISSION_TARGET_FIELDS];
+extern const char *const mission_difficulty[NUM_MISSION_DIFFICULTIES];
 
 // functions
 int mission_details_to_faction(int faction);
