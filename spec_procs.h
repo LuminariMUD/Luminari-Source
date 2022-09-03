e3e3aeed052428c440a34f0b34892bd9a5b9c140
Mosheh Ben Avraham <moshehwebservices@live.com>
2022-09-03 22:59:13 +0300

    Update spec_procs.h

--- spec_procs.h
+++ spec_procs.h
@@ -25,6 +25,7 @@ int sorted_skills[TOP_SKILL_DEFINE];
  ****************************************************************************/
 void assign_kings_castle(void);
 int do_npc_rescue(struct char_data *ch, struct char_data *friend);
+void prisoner_on_death(struct char_data *ch);
 
 /*****************************************************************************
  * Begin Functions and defines for spec_assign.c
