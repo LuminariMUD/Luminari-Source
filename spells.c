4c13b5582b03088f10083edce7aba21b72d323cc
Mosheh Ben Avraham <moshehwebservices@live.com>
2022-09-28 02:59:33 +0300

    Update spells.c

--- spells.c
+++ spells.c
@@ -405,7 +405,7 @@ void effect_charm(struct char_data *ch, struct char_data *victim,
                                        GET_LEVEL(victim) >= 8))
     send_to_char(ch, "Your victim is too powerful.\r\n");
 
-  else if (HAS_PET(ch))
+  else if (check_npc_followers(ch, NPC_MODE_COUNT, 0) >= GET_CHA_BONUS(ch))
     send_to_char(ch, "You can not manage more followers!\r\n");
 
   else if ((spellnum == SPELL_DOMINATE_PERSON || spellnum == SPELL_MASS_DOMINATION) &&
