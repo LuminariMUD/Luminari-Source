4cad4c1cccce0ad262b18d336ea125a02a0b61dd
Mosheh Ben Avraham <moshehwebservices@live.com>
2022-11-07 14:48:07 +0200

    Update staff_events.c

--- staff_events.c
+++ staff_events.c
@@ -174,8 +174,9 @@ void check_event_drops(struct char_data *killer, struct char_data *victim)
           log("SYSERR: check_event_drops() created NULL object for pristine horn");
           return;
         }
+
         obj_to_char(obj, killer); // deliver object
-        buf[MAX_STRING_LENGTH] = {'\0'};
+
         if (killer && obj && obj->short_description)
         {
           send_to_char(killer, "\tYYou have found \tn%s\tn\tY!\tn\r\n", obj->short_description);
