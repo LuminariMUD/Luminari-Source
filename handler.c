df1654add8c62660f15935da90443612bf319ccb
GickerLDS <stephen.squires@gmail.com>
2022-12-16 23:04:20 +0000

    Fixed a bug where penalties to stats from spells/effects and gear would not apply. Caster level is now character level for staff characters. Fixed a bug with siphon might applying multiple times.

--- handler.c
+++ handler.c
@@ -364,6 +364,7 @@ int calculate_best_mod(struct char_data *ch, int location, int bonus_type, int e
   int i = 0, j = 0;
   int best = 0;
   int modifier = 0;
+  int penalty = 0;
 
   /* Skip stackable bonus types and bonus types without a modifier. */
   if ((location == APPLY_NONE) ||
@@ -385,6 +386,9 @@ int calculate_best_mod(struct char_data *ch, int location, int bonus_type, int e
       continue;
 
     modifier = af->modifier;
+
+    if (modifier < 0)
+      penalty -= modifier;
 
     if (af->location == location && modifier > best)
       best = modifier;
@@ -410,13 +414,15 @@ int calculate_best_mod(struct char_data *ch, int location, int bonus_type, int e
             (GET_EQ(ch, i)->affected[j].location == location))
         {
           modifier = GET_EQ(ch, i)->affected[j].modifier;
+          if (modifier < 0)
+            penalty -= modifier;
           if (modifier > best)
             best = modifier;
         }
       }
     }
   }
-  return best;
+  return best - penalty;
 }
 
 /* this will take a character's modified 'points' and reset it
