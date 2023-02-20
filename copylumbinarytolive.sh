ec7e2df4e4822e57890ed49a38cf953c638415ad
GickerLDS <gickerlds@gmail.com>
2023-02-20 19:58:45 +0000

    Fix for lists.c

--- copylumbinarytolive.sh
+++ copylumbinarytolive.sh
@@ -1,7 +1,7 @@
 TIMEDATE=$(date +"%m-%d-%Y-%H-%M")
-mv /home/luminari/mud/bin/circle /home/luminari/mud/bin/circle.$TIMEDATE
-cp ../bin/circle /home/luminari/mud/bin
+mv /home/luminari/luminari/mud/bin/circle /home/luminari/luminari/mud/bin/circle.$TIMEDATE
+cp ../bin/circle /home/luminari/luminari/mud/bin
 echo Copied binary to luminari live port
-cp /home/luminari/mud/changelog /home/luminari/mud/lib/text/news
+cp /home/luminari/luminari/mud/changelog /home/luminari/luminari/mud/lib/text/news
 echo Moved changelog over to news file
 
