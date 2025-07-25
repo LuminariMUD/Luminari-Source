5c41861367841ee5daa84aab50baf531c540b912
Mosheh Ben Avraham <moshehwebservices@live.com>
2025-07-25 17:11:58 +0300

    Update cpbin2dev.sh

--- cpbin2dev.sh
+++ cpbin2dev.sh
@@ -4,8 +4,8 @@ cp ../bin/circle /home/luminari/dev/bin
 echo "Copied binary to luminari dev port"
 cp /home/luminari/dev/changelog /home/luminari/dev/lib/text/news
 echo "Moved changelog over to news file"
-echo "5 second delay before starting server..."
-sleep 5
+echo "3 second delay before starting server..."
+sleep 3
 echo "Starting dev server on port 4101"
 cd /home/luminari/dev && ./checkmud.sh &
 echo ""
