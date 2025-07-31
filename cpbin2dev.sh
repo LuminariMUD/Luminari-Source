ba3b9212a3f942dc7b5e078de66c0598dd190df9
LuminariMUD Developer <luminari@mud.dev>
2025-07-31 22:53:09 +0300

    fix script for old server

--- cpbin2dev.sh
+++ cpbin2dev.sh
@@ -1,11 +1,9 @@
-TIMEDATE=$(date +"%m-%d-%Y-%H-%M")
-mv /home/luminari/dev/bin/circle /home/luminari/dev/bin/circle.$TIMEDATE
 cp ../bin/circle /home/luminari/dev/bin
 echo "Copied binary to luminari dev port"
 cp /home/luminari/dev/changelog /home/luminari/dev/lib/text/news
 echo "Moved changelog over to news file"
-echo "3 second delay before starting server..."
-sleep 3
+echo "1 second delay before starting server..."
+sleep 1
 echo "Starting dev server on port 4101"
 cd /home/luminari/dev && ./checkmud.sh &
 echo ""
