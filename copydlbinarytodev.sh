89d1bac5b41c35b288e2596b5465e70892dc109f
LuminariMUD Developer <luminari@mud.dev>
2025-08-01 16:25:02 +0300

    less supression ion make file

--- copydlbinarytodev.sh
+++
@@ -1,7 +0,0 @@
-TIMEDATE=$(date +"%m-%d-%Y-%H-%M")
-mv /home/krynn/dev/bin/circle /home/krynn/dev/bin/circle.$TIMEDATE
-cp ../bin/circle /home/krynn/dev/bin
-echo Copied binary to aod reawakening dev port
-cp /home/krynn/dev/changelog /home/krynn/dev/lib/text/news
-echo Moved changelog over to news file
-
--- copydlbinarytolive.sh
+++
@@ -1,7 +0,0 @@
-TIMEDATE=$(date +"%m-%d-%Y-%H-%M")
-mv /home/krynn/live/bin/circle /home/krynn/live/bin/circle.$TIMEDATE
-cp ../bin/circle /home/krynn/live/bin
-echo Copied binary to aod reawakening live port
-cp /home/krynn/live/changelog /home/krynn/live/lib/text/news
-echo Moved changelog over to news file
-
