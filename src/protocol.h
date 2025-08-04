585500eb13600944447b6a925ca356b4e3ec9da7
LuminariMUD Developer <luminari@mud.dev>
2025-08-05 01:57:17 +0300

    .

--- src/protocol.h
+++ src/protocol.h
@@ -214,7 +214,7 @@ typedef struct descriptor_data descriptor_t;
  * @note Dragonlance and Forgotten Realms campaigns use different/no GUIs
  */
 #if !defined(CAMPAIGN_DL)
-#define MUDLET_PACKAGE "4\nhttps://luminarimud.com/download/LuminariGUI-v2.0.4.015.mpackage"
+#define MUDLET_PACKAGE "{\"version\":4,\"url\":\"https://luminarimud.com/download/LuminariGUI-v2.0.4.015.mpackage\"}"
 #endif
 
 /******************************************************************************
