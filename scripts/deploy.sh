LuminariMUD/Luminari-Source

commit 11e30feee7264ac28d54e547a23682cb6ef1620d

implemented long shot perk and luminarimud theme regions.

src/interpreter.c
@@ -4692,7 +4692,30 @@ void show_homeland_region_main_menu(struct descriptor_data *d)
 
       STATE(d) = CON_QREGION;
 #else
-     write_to_output(d, "That is not an option at this time.\r\n");
+     if (GET_REGION(d->character))
+      {
+        write_to_output(d, "\r\n\tcYou have already chosen a homeland region.  To change it you will need to ask a staff member to do it.\r\n\r\n\tn");
+        return;
+      }
+
+      write_to_output(d, "\tcRegions of Ansalon\tn\r\n\r\n");
+      int i = 1;
+      while (i < NUM_REGIONS)
+      {
+        if (!is_selectable_region(i)) {i++; continue; }
+        write_to_output(d, "%-2d) %-20s ", i, regions[i]);
+        if (((i - 1) % 3) == 2)
+          send_to_char(d->character, "\r\n");
+        i++;
+      }
+      if (((i - 1) % 3) != 2)
+        send_to_char(d->character, "\r\n");
+      write_to_output(d, "\r\n\r\nRegion selection is mainly a role playign choice, but it also awards an associated language and\r\n"
+                         "may be integrated into future game systems.\r\n");
+      write_to_output(d, "Type 'quit' to exit out of region selection.\r\n");
+      write_to_output(d, "\r\nRegion Selection (select %d for 'Abanasinia' if you do not know what to pick): ", REGION_ABANASINIA);
+
+      STATE(d) = CON_QREGION;
 #endif
 }
 

src/act.offensive.c
@@ -10742,11 +10742,9 @@ ACMD(do_fire)
   }
   else
   {
-
-    if (!IS_NPC(ch) && !HAS_FEAT(ch, FEAT_FAR_SHOT))
+    if (!IS_NPC(ch) && !HAS_FEAT(ch, FEAT_FAR_SHOT) && !has_perk(ch, PERK_RANGER_LONGSHOT))
     {
-      send_to_char(ch, "You need the 'far shot' feat to shoot outside of your"
-                       " immediate area!\r\n");
+      send_to_char(ch, "You need the 'far shot' feat or 'long shot' perk to shoot outside of your immediate area!\r\n");
       return;
     }
 
@@ -10850,6 +10848,12 @@ ACMD(do_fire)
 
     if (ch && vict && IN_ROOM(ch) != IN_ROOM(vict))
     {
+      /* Combined bonus: if player has Far Shot feat AND Longshot perk, +3 to attack when firing outside their room */
+      if (!IS_NPC(ch) && HAS_FEAT(ch, FEAT_FAR_SHOT) && has_perk(ch, PERK_RANGER_LONGSHOT))
+      {
+        GET_TEMP_ATTACK_ROLL_BONUS(ch) += 3;
+      }
+
       hit(ch, vict, TYPE_UNDEFINED, DAM_RESERVED_DBC, 0, 2); // 2 in last arg indicates ranged
       /* don't forget to remove the fight event! */
       if (char_has_mud_event(ch, eCOMBAT_ROUND))

src/constants.c
@@ -6448,42 +6448,19 @@ CHECK_TABLE_SIZE(cities, NUM_CITIES + 1);
 // region names
 const char *regions[] = {
     "none",
-    "Aglarond",
-    "Amn",
-    "Anauroch Desert",
-    "Calimshan",
-    "Chessenta",
-    "Chult",
-    "Cormyr",
-    "Dambrath",
-    "Elfharrow",
-    "Evermeet",
-    "Halruaa",
-    "Icewind Dale",
-    "Impilitur",
-    "Kara-Tur",
-    "Luiren",
-    "Luruar",
-    "Mulhorand",
-    "Rashemen",
-    "Sembia",
-    "Tethyr",
-    "Thay",
-    "Thesk",
-    "The Cold Lands",
-    "The Dalelands",
-    "The Hordelands",
-    "The Lake of Steam",
-    "The Moonsea",
-    "The Savage Frontier",
-    "The Sword Coast",
-    "The Underdark",
-    "Turmish",
-    "Tymanther",
-    "Unther",
-    "Westgate",
-    "Zakhara",
-    "Outer Planes",
+    "Ashenport",
+    "Sanctus",
+    "Onduis",
+    "Selerish",
+    "Carstan",
+    "Axtros",
+    "Hir",
+    "Quechian",
+    "Vailand",
+    "Oorpii",
+    "Kellust",
+    "East Ubdina",
+    "West Ubdina",
     "\n"};
 CHECK_TABLE_SIZE(regions, NUM_REGIONS + 1);
 

src/structs.h
@@ -989,44 +989,20 @@
 #else
 
 #define REGION_NONE                 0
-#define REGION_AGLAROND             1
-#define REGION_AMN                  2
-#define REGION_ANAUROCH_DESERT      3
-#define REGION_CALIMSHAN            4
-#define REGION_CHESSENTA            5
-#define REGION_CHULT                6
-#define REGION_CORMYR               7
-#define REGION_DAMBRATH             8
-#define REGION_ELFHARROW            9
-#define REGION_EVERMEET             10
-#define REGION_HALRUAA              11
-#define REGION_ICEWIND_DALE         12
-#define REGION_IMPILTUR             13
-#define REGION_KARA_TUR             14
-#define REGION_LUIREN               15
-#define REGION_LURUAR               16
-#define REGION_MULHORAND            17
-#define REGION_RASHEMEN             18
-#define REGION_SEMBIA               19
-#define REGION_TETHYR               20
-#define REGION_THAY                 21
-#define REGION_THESK                22
-#define REGION_THE_COLD_LANDS       23
-#define REGION_THE_DALELANDS        24
-#define REGION_THE_HORDELANDS       25
-#define REGION_THE_LAKE_OF_STEAM    26
-#define REGION_THE_MOONSEA          27
-#define REGION_THE_SAVAGE_FRONTIER  28
-#define REGION_THE_SWORD_COAST      29
-#define REGION_THE_UNDERDARK        30
-#define REGION_TURMISH              31
-#define REGION_TYMANTHER            32
-#define REGION_UNTHER               33
-#define REGION_WESTGATE             34
-#define REGION_ZAKHARA              35
-#define REGION_OUTER_PLANES         36
-
-#define NUM_REGIONS                 37
+#define REGION_ASHENPORT            1
+#define REGION_SANCTUS              2
+#define REGION_ONDUIS               3
+#define REGION_SELERISH             4
+#define REGION_CARSTAN              5
+#define REGION_AXTROS               6
+#define REGION_HIR                  7
+#define REGION_QUECHIAN             8
+#define REGION_VAILAND              9
+#define REGION_OORPII               10
+#define REGION_KELLUST              11
+#define REGION_EAST_UBDINA          12
+#define REGION_WEST_UBDINA          13
+#define NUM_REGIONS                 14
 
 #endif
 

commit 0f563251647b68e5435373cf7c319f770023d3fc

Fix build errors from region refactoring and update deployment script
Fixed multiple compilation errors introduced in commit 11e30fee:
- Fixed MySQL/MariaDB header paths (changed mysql/mysql.h to mariadb/mysql.h)
- Fixed REGION constant references by adding proper campaign conditionals
- Separated DragonLance, Forgotten Realms, and LuminariMUD region code paths
- Added libjson-c-dev dependency to deployment script for all OS types

Files modified:
- src/mysql_boards.h, src/mysql_boards.c, src/bedit.c (MySQL headers)
- src/race.c (campaign separation for region info/language functions)
- src/utils.c (REGION_OUTER_PLANES conditional)
- src/act.wizard.c, src/interpreter.c (region selection prompts)
- scripts/deploy.sh (added json-c dependency)
- docs/DEPLOYMENT_LOG.md (comprehensive deployment documentation)

Build now completes successfully. All tests passed.

scripts/deploy.sh
@@ -99,7 +99,7 @@ install_dependencies() {
             sudo apt-get install -y \
                 build-essential cmake autoconf automake libtool pkg-config \
                 libcrypt-dev libgd-dev libmariadb-dev libcurl4-openssl-dev \
-                libssl-dev mariadb-server git make
+                libssl-dev libjson-c-dev mariadb-server git make
             
             if [[ "$BUILD_TYPE" == "development" ]]; then
                 sudo apt-get install -y gdb valgrind
@@ -111,7 +111,7 @@ install_dependencies() {
             sudo yum install -y \
                 gcc gcc-c++ make cmake autoconf automake libtool \
                 mariadb mariadb-devel mariadb-server \
-                gd-devel openssl-devel libcurl-devel git
+                gd-devel openssl-devel libcurl-devel json-c-devel git
             
             if [[ "$BUILD_TYPE" == "development" ]]; then
                 sudo yum install -y gdb valgrind
@@ -122,7 +122,7 @@ install_dependencies() {
             print_msg "$GREEN" "Installing packages for Arch Linux..."
             sudo pacman -Sy --noconfirm \
                 base-devel cmake mariadb libmariadbclient \
-                gd openssl curl git
+                gd openssl curl json-c git
             
             if [[ "$BUILD_TYPE" == "development" ]]; then
                 sudo pacman -Sy --noconfirm gdb valgrind

src/act.wizard.c
@@ -4691,7 +4691,11 @@ static int perform_set(struct char_data *ch, struct char_data *vict, int mode, c
 #if defined(CAMPAIGN_DL)
   send_to_char(ch, "\r\nRegion Selection (select %d for 'Abanasinia' if you do not know what to pick): ", REGION_ABANASINIA);
 #else      
+#if defined(CAMPAIGN_FR)
       send_to_char(ch, "\r\nRegion Selection (select %d for 'Sword Coast' if you do not know what to pick): ", REGION_THE_SWORD_COAST);
+#else
+      send_to_char(ch, "\r\nRegion Selection (select %d for default if you do not know what to pick): ", REGION_NONE);
+#endif
 #endif
     }
     else

src/bedit.c
@@ -16,7 +16,7 @@
 #include "genolc.h"
 #include "interpreter.h"
 #include "helpers.h"
-#include "mysql/mysql.h"
+#include "mariadb/mysql.h"
 #include "mysql_boards.h"
 
 /*-------------------------------------------------------------------*/

src/interpreter.c
@@ -3534,8 +3534,10 @@ switch (load_result)
       write_to_output(d, "Type 'quit' to exit out of region selection.\r\n");
 #ifdef CAMPAIGN_DL
       write_to_output(d, "\r\nRegion Selection (select %d for 'Abanasinia' if you do not know what to pick): ", REGION_ABANASINIA);
-#else
+#elif defined(CAMPAIGN_FR)
       write_to_output(d, "\r\nRegion Selection (select %d for 'Sword Coast' if you do not know what to pick): ", REGION_THE_SWORD_COAST);
+#else
+      write_to_output(d, "\r\nRegion Selection (select %d for default if you do not know what to pick): ", REGION_NONE);
 #endif
       STATE(d) = CON_QREGION;
       return;
@@ -4688,7 +4690,13 @@ void show_homeland_region_main_menu(struct descriptor_data *d)
       write_to_output(d, "\r\n\r\nRegion selection is mainly a role playign choice, but it also awards an associated language and\r\n"
                          "may be integrated into future game systems.\r\n");
       write_to_output(d, "Type 'quit' to exit out of region selection.\r\n");
+#if defined(CAMPAIGN_DL)
       write_to_output(d, "\r\nRegion Selection (select %d for 'Abanasinia' if you do not know what to pick): ", REGION_ABANASINIA);
+#elif defined(CAMPAIGN_FR)
+      write_to_output(d, "\r\nRegion Selection (select %d for 'Sword Coast' if you do not know what to pick): ", REGION_THE_SWORD_COAST);
+#else
+      write_to_output(d, "\r\nRegion Selection (select %d for default if you do not know what to pick): ", REGION_NONE);
+#endif
 
       STATE(d) = CON_QREGION;
 #else
@@ -4713,7 +4721,13 @@ void show_homeland_region_main_menu(struct descriptor_data *d)
       write_to_output(d, "\r\n\r\nRegion selection is mainly a role playign choice, but it also awards an associated language and\r\n"
                          "may be integrated into future game systems.\r\n");
       write_to_output(d, "Type 'quit' to exit out of region selection.\r\n");
+#if defined(CAMPAIGN_DL)
       write_to_output(d, "\r\nRegion Selection (select %d for 'Abanasinia' if you do not know what to pick): ", REGION_ABANASINIA);
+#elif defined(CAMPAIGN_FR)
+      write_to_output(d, "\r\nRegion Selection (select %d for 'Sword Coast' if you do not know what to pick): ", REGION_THE_SWORD_COAST);
+#else
+      write_to_output(d, "\r\nRegion Selection (select %d for default if you do not know what to pick): ", REGION_NONE);
+#endif
 
       STATE(d) = CON_QREGION;
 #endif

src/mysql_boards.c
@@ -27,7 +27,7 @@
 #include "deities.h"
 #include "helpers.h"
 
-#include "mysql/mysql.h"
+#include "mariadb/mysql.h"
 #include "mysql_boards.h"
 
 /* External Variables */

src/mysql_boards.h
@@ -11,7 +11,7 @@
 #ifndef __MYSQL_BOARDS_H__
 #define __MYSQL_BOARDS_H__
 
-#include "mysql/mysql.h"
+#include "mariadb/mysql.h"
 
 /* Board System Constants */
 #define MAX_BOARD_TITLE_LENGTH    256

src/race.c
@@ -5202,7 +5202,7 @@ const char *get_region_info(int region)
   }
   return "Undefined";
 }
-#else
+#elif defined(CAMPAIGN_FR)
 const char *get_region_info(int region)
 {
   switch (region)
@@ -5311,6 +5311,12 @@ const char *get_region_info(int region)
   }
   return "Undefined";
 }
+#else
+/* Default LuminariMUD campaign - region info not yet implemented */
+const char *get_region_info(int region)
+{
+  return "This region's detailed information is not yet available.";
+}
 #endif
 #if defined(CAMPAIGN_DL)
 
@@ -5347,7 +5353,7 @@ int get_region_language(int region)
   }
   return LANG_COMMON;
 }
-#else
+#elif defined(CAMPAIGN_FR)
 
 int get_region_language(int region)
 {
@@ -5432,6 +5438,12 @@ int get_region_language(int region)
     return SKILL_LANG_COMMON;
   }
   return SKILL_LANG_COMMON;
+}
+#else
+/* Default LuminariMUD campaign - region language not yet implemented */
+int get_region_language(int region)
+{
+  return LANG_COMMON;
 }
 #endif
 

src/utils.c
@@ -9970,8 +9970,10 @@ bool is_selectable_region(int region)
 {
   switch (region)
   {
+#if defined(CAMPAIGN_DL)
     case REGION_OUTER_PLANES:
       return false;
+#endif
   }
   return true;
 }